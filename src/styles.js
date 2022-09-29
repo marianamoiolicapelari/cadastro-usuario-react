e9a04fcd56be33b9f887a870863e4927d4dda052
Mariana M.C <marianamoioli@gmail.com>
2022-09-29 08:17:01 -0300

    first part of the finished project

--- src/styles.js
+++ src/styles.js
@@ -9,7 +9,7 @@ export const Container = styled.div`
     flex-direction: column;
     align-items: center;
     gap: 40px;
-    height: 100vh;
+    //height: 100vh;
 `
 
 export const Image = styled.img`
@@ -88,5 +88,33 @@ export const Button = styled.button`
 
     &:active {
         opacity: 0.5;
+    }
+`
+
+export const User = styled.li `
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 20px;
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 14px;
+    width: 342px;
+    height: 58px;
+    border: none;
+    outline: none;
+    
+    p {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 28px;
+        color: #FFFFFF;
+    }
+
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;
     }
 `
