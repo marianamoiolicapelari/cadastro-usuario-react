56bfd306241cd7390592d866ff4fddf80e8de9a5
Mariana M.C <marianamoioli@gmail.com>
2023-03-21 15:00:39 -0300

    Update changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, { useState, useRef } from 'react'
-
-import axios from 'axios'
+import React, { useState, useRef } from 'react';
 
 import People from './assets/people.svg'
 import Arrow from './assets/arrow.svg'
@@ -17,20 +15,20 @@ import {
   User
 } from './styles'
 
-function App() {
+const App = () => {
   const [users, setUsers] = useState([])
   const inputName = useRef()
   const inputAge = useRef()
 
-
-  async function addNewUser() {
-
-    const data = await axios.post("http://localhost:3001/users", {
-      name: inputName.current.value,
-      age: inputAge.current.value,
-    })
-
-      console.log(data)
+  function addNewUser() {
+    setUsers([
+      ...users,
+      {
+        id: Math.random(),
+        name: inputName.current.value,
+        age: inputAge.current.value
+      }
+    ])
   }
 
   function deleteUser(userId) {
@@ -41,30 +39,34 @@ function App() {
 
   return (
     <Container>
-      <Image alt="logo-imagem" src={People} />
+      <Image alt='logo-imagem' src={People} />
       <ContainerItens>
-        <H1> Olá! </H1>
+        <H1>Olá!</H1>
 
         <InputLabel>Nome</InputLabel>
-        <Input ref={inputName} placeholder="Nome" />
+        <Input ref={inputName} placeholder='Nome' />
 
         <InputLabel>Idade</InputLabel>
-        <Input ref={inputAge} placeholder="Idade" />
+        <Input ref={inputAge} placeholder='Idade' />
 
         <Button onClick={addNewUser}>
-          Cadastrar <img alt="seta" src={Arrow} />
+          Cadastrar<img alt='seta' src={Arrow} />
         </Button>
 
         <ul>
-          {users.map((user) => (
+          {users.map(user => (
             <User key={user.id}>
-              <p>{user.name}</p> <p>{user.age}</p>
+              <p>{user.name}</p><p>{user.age}</p>
               <button onClick={() => deleteUser(user.id)}>
-                <img src={Trash} alt="lata-de-lixo" />
+                <img alt='lata-de-lixo' src={Trash} />
               </button>
             </User>
-          ))}
+
+          ))
+          }
         </ul>
+
+
       </ContainerItens>
     </Container>
   )
--- src/styles.js
+++ src/styles.js
@@ -1,114 +1,119 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 import Background from './assets/background1.svg'
 
 export const Container = styled.div`
-    background: url("${Background}");
-    background-size: cover;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 40px;
+   height: max-content;
+   background: url('${Background}');
+   background-size: cover;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   gap: 40px;
 `
-
 export const Image = styled.img`
-    margin-top: 30px;
+   margin-top: 30px;
 `
-
 export const ContainerItens = styled.div`
-    background: linear-gradient(157.44deg, rgba(255, 255, 255, 0.6) 0.84%, rgba(255, 255, 255, 0.6) 0.85%, rgba(255, 255, 255, 0.15) 100%);
-    border-radius: 61px 61px 0px 0px;
-    padding: 50px 36px;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
+   height: 100vh;
+   background: linear-gradient(
+      157.44deg, 
+      rgba(255, 255, 255, 0.6) 0.84%,
+      rgba(255, 255, 255, 0.6) 0.85%, 
+      rgba(255, 255, 255, 0.15) 100%);
+   border-radius: 61px 61px 0px 0px;
+   padding: 50px 36px;
+   display: flex;
+   flex-direction: column;
 `
-
 export const H1 = styled.h1`
-    font-style: normal;
-    font-weight: 700;
-    font-size: 34px;
-    line-height: 40px;
-    text-align: center;
-    color: #FFF;
-    margin-bottom: 80px;
+   font-style: normal;
+   font-weight: 700;
+   font-size: 34px;
+   line-height: 40px;
+   text-align: center;
+   color: #FFFFFF;
+   margin-bottom: 80px;
+    
 `
-
 export const InputLabel = styled.p`
-    letter-spacing: -0.408px;
-    color: #EEEEEE;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 22px;
-    margin-left: 25px;
+   letter-spacing: -0.408px;
+   color: #EEEEEE;
+   font-style: normal;
+   font-weight: 700;
+   font-size: 18px;
+   line-height: 22px;
+   margin-left: 25px;
 `
 
 export const Input = styled.input`
-    background: rgba(255, 255, 255, 0.25);
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 14px;
-    width: 342px;
-    height: 58px;
-    border: none;
-    outline: none;
-    padding-left: 25px;
-    margin-bottom: 35px;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 28px;
-    color: #FFF;
+   background: rgba(255, 255, 255, 0.25);
+   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+   border-radius: 14px;
+   border: none;
+   outline: none;
+   width: 342px;
+   height: 58px;
+   padding-left: 25px;
+   font-style: normal;
+   font-weight: 400;
+   font-size: 20px;
+   line-height: 28px;
+   color: #FFFFFF;
+   margin-bottom: 34px;
 `
 
 export const Button = styled.button`
-    width: 342px;
-    height: 74px;
-    background: rgba(0, 0, 0, 0.8);
-    border-radius: 14px;
-    border: none;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 17px;
-    line-height: 28px;
-    color: #FFF;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 20px;
+   width: 342px;
+   height: 74px;
+   background: rgba(0, 0, 0, 0.8);
+   border-radius: 14px;
+   border: none;
+   font-style: normal;
+   font-weight: 700;
+   font-size: 17px;
+   line-height: 28px;
+   color: #FFFFFF;
+   cursor: pointer;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   gap: 20px;
 
-    &:hover {
-        opacity: 0.8;
-    }
-    &:active {
-        opacity: 0.5;
-    }
-`
+   &:hover {
+      opacity: 0.8;
+   }
 
-export const User = styled.li `
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    margin-top: 20px;
-    background: rgba(255, 255, 255, 0.25);
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 14px;
-    width: 342px;
-    height: 58px;
-    border: none;
-    outline: none;
-    
-    p {
-        font-style: normal;
-        font-weight: 400;
-        font-size: 24px;
-        line-height: 28px;
-        color: #FFFFFF;
-    }
+   &:active {
+      opacity: 0.5;
+   }
+` 
 
-    button {
-        background: none;
-        border: none;
-        cursor: pointer;
-    }
+export const User = styled.li`
+   display: flex;
+   justify-content: space-around;
+   align-items: center;
+   margin-top: 20px;
+   background: rgba(255, 255, 255, 0.25);
+   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+   border-radius: 14px;
+   border: none;
+   outline: none;
+   width: 342px;
+   height: 58px;
+   
+   p {
+      font-style: normal;
+      font-weight: normal;
+      font-size: 20px;
+      line-height: 24px;
+      color: #FFFFFF;
+   }
+
+   button {
+      background: none;
+      border: none;
+      cursor: pointer;
+   }
+
+ 
 `
